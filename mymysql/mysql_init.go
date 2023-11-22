914923b3062aebf75d5275e1043e34787e0a5cc2
JiangJiaP <716023618@QQ.COM>
2023-11-22 16:56:54 +0800

    d

--- mymysql/mysql_init.go
+++ mymysql/mysql_init.go
@@ -49,12 +49,12 @@ func CIdSearchByMid(data model.Data) ([]model.Data, error) {
 	var redata []model.Data
 	var err error
 	if data.DeviceId == "" {
-		err = Db.Select(redata,"select * from id_info where multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ?", "%"+data.UserId+"%", "%"+data.DataId+"%", "%"+data.MacId+"%", "%"+data.ServiceId+"%")
+		err = Db.Select(&redata,"select * from id_info where multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ?", "%"+data.UserId+"%", "%"+data.DataId+"%", "%"+data.MacId+"%", "%"+data.ServiceId+"%")
 		if err != nil {
 			fmt.Printf("MultiIdSearch have wrong")
 		}
 	}else {
-		err = Db.Select(redata,"select * from id_info where multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ?", "%"+data.UserId+"%", "%"+data.DataId+"%", "%"+data.MacId+"%", "%"+data.ServiceId+"%","%"+data.DeviceId+"%")
+		err = Db.Select(&redata,"select * from id_info where multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ? and multi_id LIKE ?", "%"+data.UserId+"%", "%"+data.DataId+"%", "%"+data.MacId+"%", "%"+data.ServiceId+"%","%"+data.DeviceId+"%")
 		if err != nil {
 			fmt.Printf("MultiIdSearch have wrong")
 		}
